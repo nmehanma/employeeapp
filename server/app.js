c1e776e2589b6845af228193368fd1d609cc4889
nmehanma <nmehanma@gmail.com>
2022-01-16 01:06:44 -0500

    making Schema and connecting to mongodb

--- server/app.js
+++ server/app.js
@@ -2,15 +2,33 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("./Employee");
+const dotenv = require('dotenv')
+dotenv.config()
 
+const Employee = mongoose.model("employee");
 
+const dbUrl = process.env.DB_URL;
 
-const dbUrl = process.env.DB_URL
+mongoose.connect(dbUrl, {
+  useNewURLParser: true,
+  useUnifiedTopology:true
+
+});
+
+mongoose.connection.on("connected",()=>{
+  console.log("Database Connected")
+})
+
+mongoose.connection.on("error",()=>{
+  console.log("error", err)
+})
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to node js");
 });
 
 app.listen(3000, () => {
-  console.log("server is running");
+  console.log("Server is running");
 });
