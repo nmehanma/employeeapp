526c93df954d9ae2cda7de8263be842d9e4d4b2e
nmehanma <nmehanma@gmail.com>
2022-01-16 16:26:06 -0500

    making the post data route holder

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("./Employee");
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(bodyParser.json());
 
 const Employee = mongoose.model("employee");
 
@@ -12,21 +14,41 @@ const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
   useNewURLParser: true,
-  useUnifiedTopology:true
-
+  useUnifiedTopology: true
 });
 
-mongoose.connection.on("connected",()=>{
-  console.log("Database Connected")
-})
+mongoose.connection.on("connected", () => {
+  console.log("Database Connected");
+});
 
-mongoose.connection.on("error",()=>{
-  console.log("error", err)
-})
-
+mongoose.connection.on("error", err => {
+  console.log("error", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to node js");
+});
+
+app.post("/send-data", (req, res) => {
+  const employee = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.picture,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    positon: req.body.position
+  });
+  // console.log(req.body)
+  employee
+  .save()
+  .then(data => {
+    console.log(data);
+    res.send("success");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  // res.send("success")
 });
 
 app.listen(3000, () => {
